3015d7a9c2ae18ad13ea49333d9cb5512ff1a879
Ghabriel <ghabriel.nunes@hotmail.com>
2016-07-02 11:15:47 -0300

    UI updates, including the creation of the Analysis Table

--- Workspace.js
+++ Workspace.js
@@ -12,13 +12,73 @@ var buttonBar = function() {
 	return $("#button_bar");
 };
 
+var cfgContainer = function() {
+	return $("#current_cfg");
+};
+
 var node = function(tag) {
 	return document.createElement(tag);
+};
+
+var genCell = function(content) {
+	var cell = node("td");
+	cell.innerHTML = content;
+	return cell;
 };
 
 window.Workspace = function() {
 	var self = this;
 	this.currentCFG = null;
+
+	// Analyzes the current CFG of this workspace, printing informations such
+	// as recursion data, factoring data, first, follow and parsing table.
+	function printAnalysisTable() {
+		var table = node("table");
+		var headerRow = node("tr");
+		var header = node("th");
+		header.innerHTML = "Analysis";
+		header.colSpan = 5;
+		headerRow.appendChild(header);
+		table.appendChild(headerRow);
+
+		var row, cell;
+		row = node("tr");
+		var fields = ["Non-Terminal", "Recursive", "Factored", "First", "Follow"];
+		for (var i = 0; i < fields.length; i++) {
+			cell = node("th");
+			cell.innerHTML = fields[i];
+			row.appendChild(cell);
+		}
+		table.appendChild(row);
+
+		var cfg = self.currentCFG;
+		var nonTerminals = cfg.getNonTerminals();
+		var first = cfg.first();
+		var recursionInfo = cfg.getRecursionInformation();
+		var factorizationInfo = cfg.getFactorizationInformation();
+		var recursiveNT = recursionInfo.recursiveNonTerminals;
+		var nonFactoredNT = factorizationInfo.nonFactoredNonTerminals;
+		for (var i = 0; i < nonTerminals.length; i++) {
+			var name = nonTerminals[i];
+			row = node("tr");
+			row.appendChild(genCell(name));
+			row.appendChild(genCell(""));
+			row.appendChild(genCell(""));
+			//row.appendChild(genCell(recursiveNT.includes(name) ? "Yes" : "No"));
+			//row.appendChild(genCell(nonFactoredNT.includes(name) ? "No" : "Yes"));
+			row.appendChild(genCell(first[name].join(", ")));
+			row.appendChild(genCell(""));
+			table.appendChild(row);
+		}
+		container().appendChild(table);
+	}
+
+	// Updates the UI, replacing all previous content with the informations
+	// about the current CFG.
+	function updateUI() {
+		container().innerHTML = "";
+		printAnalysisTable();
+	}
 
 	// Shows an error to the user.
 	this.error = function(message) {
@@ -35,13 +95,8 @@ window.Workspace = function() {
 			return false;
 		}
 		self.currentCFG = instance;
-
-		var first = instance.first();
-		for (var name in first) {
-			if (first.hasOwnProperty(name)) {
-				console.log(name + ": " + first[name].join(", "));
-			}
-		}
+		cfgContainer().innerHTML = cfg;
+		updateUI();
 		return true;
 	};
 
